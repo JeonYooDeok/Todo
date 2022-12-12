4117884d956023602e4cdb69a1277274b992d5d1
JeonYooDeok <jyd910215@gmail.com>
2022-12-12 17:44:58 +0900

    핫모듈 리로드

--- todo-app/vite.config.js
+++ todo-app/vite.config.js
@@ -2,7 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	//핫모듈리로드
+	server: {
+		watch: {
+			usePolling: true
+		}
+	}
+	
+	//핫모듈리로드
 };
 
 export default config;
